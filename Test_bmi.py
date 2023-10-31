653a5f229df7db01c9e44e0d3e959307dabaa46d
Elroy Ong <ELROYONG.22@ichat.sp.edu.sg>
2023-10-31 11:08:03 +0800

    testing check

---
+++ Test_bmi.py
@@ -0,0 +1,16 @@
+import Lab2.bmi as bmi
+import pytest
+def test_bmi_normal_weight():
+    # Test the BMI classification for Normal Weight
+    result = bmi.calculate_bmi(height=1.73, weight=70)
+    assert result[1] == "Normal Weight"
+
+def test_bmi_over_weight():
+    # Test the BMI classification for Over Weight
+    result = bmi.calculate_bmi(height=1.73, weight=80)
+    assert result[1] == "Over Weight"
+
+def test_bmi_under_weight():
+    # Test the BMI classification for Under Weight
+    result = bmi.calculate_bmi(height=1.73, weight=50)
+    assert result[1] == "Under Weight"
